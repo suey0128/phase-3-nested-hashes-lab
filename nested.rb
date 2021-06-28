109f84b98a07787448266c563f5822f5caeb6d81
suey0128 <syui0128@gmail.com>
2021-06-28 15:39:23 -0700

    commit tree 312a4794a4bf346959313ac189822459dc78bdd2

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,7 +37,10 @@ def alan_kay_is_known_for
       }
     }
 
+  programmer_hash[:alan_kay][:known_for]
 end
+
+alan_kay_is_known_for
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,7 +58,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,6 +85,11 @@ def adding_matz
       }
     }
 
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  programmer_hash
 end
 
 def changing_alan
@@ -103,7 +111,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +134,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 end
